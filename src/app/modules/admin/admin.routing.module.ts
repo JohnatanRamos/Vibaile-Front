81f618aa588dc28b635f09b8ba3da3c6f60abf17
Johnatan R <johnatan.r1000@gmail.com>
2021-08-19 22:04:18 -0500

    Se agrega el login

--- src/app/modules/admin/admin.routing.module.ts
+++ src/app/modules/admin/admin.routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('./modules/admin/admin.module').then((m) => m.AdminModule),
+    component: LoginComponent,
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AdminRoutingModule {}
